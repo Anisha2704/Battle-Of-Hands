4841e9a54aa6914d756f482cf17a7bc299d2e763
Anisha2704 <anishamalore45@gmail.com>
2025-06-09 09:10:56 -0700

    landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,46 @@
+// 
+
+
+import React, { useState } from "react";
+import "./App.css";
+import Card  from "./Card";
+import One from "./One";
 
 function App() {
   
+  let [mode,setMode] = useState("");
+
+  const single = () => {
+    setMode("single");
+  }
+  const two = () => {
+    setMode("two");
+  }
+
   return (
     <>
-      
+      {mode === "single" ? (
+        <One />
+      ) : (
+        <>
+          <h1>Welcome to Rock, Paper, Scissors!</h1>
+          <h1>✌ 👊 🤜</h1>
+          <p>
+            Step into the ultimate battleground where hands speak louder than words!
+            Are you ready to throw down your best move and outsmart your opponent?
+            This isn’t just a game — it’s a duel of instinct, speed, and strategy.
+            Whether you're challenging a cold, calculating machine or testing a
+            friend's reflexes, the fate of your fingers awaits.
+          </p>
+          
+          <div className="card-container">
+            <Card onClick={single} title="Single Player" descp="Think you can outplay the machine? Take on the AI and prove you're the hand-to-hand champion!" />
+            <Card onClick={two} title="Two Player" descp="Grab a friend and settle the score in the arena — side by side, turn by turn!" />
+          </div>
+        </>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Card.jsx
@@ -0,0 +1,16 @@
+import './Card.css';
+
+const Card = (props) => {
+  return (
+    <div class="card" onClick={props.onClick}>
+        <div class="card-body">
+            <h4 class="card-title">{props.title}</h4>
+            <p class="card-text">{props.descp}</p>
+            
+        </div>
+    </div>
+    
+  )
+}
+
+export default Card
